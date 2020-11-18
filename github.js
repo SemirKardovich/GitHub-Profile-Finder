e6ce213b8f032459f7debaf3b9c46544ca8a854c
Semir Kardovikj <semir.k1612@gmail.com>
2020-11-18 02:30:06 +0100

    fetch url changed

--- github.js
+++ github.js
@@ -6,7 +6,7 @@ class GitHub {
     }
 
     async getUser(user) {
-        const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profileData = await profileResponse.json();
 
